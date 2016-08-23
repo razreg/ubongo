de402da80adaec172d10baed525d2ce171cf8fa5
shellybar <bar.shelley@gmail.com>
2016-08-23 13:13:24 +0300

    Machine changes

--- infra/common/src/main/java/ubongo/common/constants/MachineConstants.java
+++ infra/common/src/main/java/ubongo/common/constants/MachineConstants.java
@@ -4,7 +4,7 @@ public class MachineConstants {
 
     public static final String SERVER_FALLBACK = "nova.cs.tau.ac.il";
 
-    public static final String BASE_UNIT_REQUEST = "Base unit task";
+    public static final String BASE_UNIT_REQUEST = "Unit task";
     public static final String KILL_TASK_REQUEST = "Kill unit task";
     public static final String UPDATE_TASK_REQUEST = "Update task status";
     public static String GET_MACHINE_PERFORMANCE = "Get machine performance request";
